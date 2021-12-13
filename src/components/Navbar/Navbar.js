290205edb2494516d532290bd9d3f673415d0d7a
EhsanAlimo <ehsan.russell69@gmail.com>
2021-12-13 20:32:29 +1100

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./navbar.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import DocumentScannerIcon from "@mui/icons-material/DocumentScanner";
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <div className="page-nav-container">
+          <div className="headshot">
+            <img
+              src="https://thethirdwave.co/app/themes/ttw/assets/img/third-wave-logo-color.svg"
+              alt="Ehsan Alimo"
+            />
+          </div>
+          <div className="middle__icons">
+            <div className="home">
+              <p>About</p>
+              <div className="home__icon">
+                <HomeIcon />
+              </div>
+            </div>
+            <div className="home">
+              <p>Projects</p>
+              <div className="project__icon">
+                <WorkIcon />
+              </div>
+            </div>
+            <div className="home">
+              <p>Contact</p>
+              <div className="contact__icon">
+                <ContactsIcon />
+              </div>
+            </div>
+            <div className="home">
+              <p>Resume</p>
+              <div className="resume__icon">
+                <DocumentScannerIcon />
+              </div>
+            </div>
+          </div>
+          <div className="social__media">
+            <GitHubIcon />
+            <LinkedInIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
